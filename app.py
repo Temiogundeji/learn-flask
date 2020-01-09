be0f140339558c96b2556a716908cf3e6e208189
Temiogundeji <yusuffogundeji@gmail.com>
2020-01-09 02:17:25 +0100

    static and dynamic routing

--- app.py
+++ app.py
@@ -4,5 +4,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World"
+
+@app.route("/<my_name>")
+def greetings(my_name):
+    return "Welcome" + my_name + "!"
+
+@app.route('/square/<int:number>')
+def show_square(number):
+    return "Square of" + str(number * number) + " is:"
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port =3000)
