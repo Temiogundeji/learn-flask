4562ef43fb692552a7ad254581862a198941159d
Temiogundeji <yusuffogundeji@gmail.com>
2020-01-10 19:23:21 +0100

    static file added

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask
+"""Flask app for paw rescue center"""
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World"
+def home():
+    return render_template('home.html')
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
-@app.route("/<my_name>")
-def greetings(my_name):
-    return "Welcome" + my_name + "!"
-
-@app.route('/square/<int:number>')
-def show_square(number):
-    return "Square of" + str(number * number) + " is:"
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port =3000)
