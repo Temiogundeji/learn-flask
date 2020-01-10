6b23db9e50c2f0b87e31ea0a092a1e248457bc6c
Temiogundeji <yusuffogundeji@gmail.com>
2020-01-10 19:32:05 +0100

    using dynamic templates

--- app.py
+++ app.py
@@ -2,9 +2,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+Users = {
+        "Archie":"Amsterdam", 
+        "Veronica":"London", 
+        "Betty":"San Francisco", 
+        "Jughead":"Los Angeles"
+    }
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', users=Users)
 @app.route("/about")
 def about():
     return render_template("about.html")
