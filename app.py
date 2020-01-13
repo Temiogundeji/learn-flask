2397bf955b6dc5d47aae87a70277a7e079f930e0
Temiogundeji <yusuffogundeji@gmail.com>
2020-01-13 15:28:09 +0100

    template inheritance

--- app.py
+++ app.py
@@ -22,7 +22,17 @@ def about():
     """View function for About Page."""
     return render_template("about.html")
 
-
+@app.route("/pet-details/<int:pet_id>")
+def pet_details(pet_id):
+    # for pet in pets:
+    #     if pet['id'] == pet_id:
+    #         return render_template("pet-details.html", pet = pet)
+    #         url_for('pet_details', pet_id=pet['id'])
+    #     abort(404, description="No Pet was Found with the given ID")   
+    pet = next((pet for pet in pets if pet["id"] == pet_id), None) 
+    if pet is None: 
+        abort(404, description="No Pet was Found with the given ID")
+    return render_template("pet-details.html", pet = pet)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
